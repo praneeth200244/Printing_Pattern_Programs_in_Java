1dfa4b3b736c3814881108bbddb4898a89defecb
praneeth200244 <106474034+praneeth200244@users.noreply.github.com>
2022-10-26 10:51:27 +0530

    Add files via upload

---
+++ Pattern_13.java
@@ -0,0 +1,31 @@
+public class Pattern_13 {
+    public static void main(String[] args) {
+        for (int i=1; i<=8; i++) {
+            for (int j=1; j<=(8-i); j++) {
+                System.out.print("  ");
+            }
+            for (int k=1; k<(2*i); k++) {
+                if ((k>1) && (k<2*i-1)) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("\n");
+
+        int rows = 8;
+        for (int i=1; i<=rows; i++) {
+            for (int j=1; j<(2*rows); j++) {
+                if ((i == j) || (i+j == (2*rows))) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ Pattern_14.java
@@ -0,0 +1,28 @@
+public class Pattern_14 {
+    public static void main(String[] args) {
+        int rows=15;
+        for (int i=1; i<=rows; i++) {
+            for (int j=1; j<(2*rows); j++) {
+                if ((i+j == rows+1) || (j-i == rows-1) || (i == rows)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        rows = 15;
+        for (int i=1; i<=rows; i++) {
+            for (int j=1; j<(2*rows); j++) {
+                if ((i == j) || (i == 1) || (i+j == (2*rows))) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
